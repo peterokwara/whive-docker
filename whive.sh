a5d9f79e523e0720be242e08354b7f4b225c36d3
Peter Okwara <peter.okwara@outlook.com>
2020-09-28 00:56:12 +0300

    Add new configuration and setup for working dockerfiles

---
+++ whive.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# TODO run a node
+# whived
+
+# TODO check balance
+# whive-cli
+
+# TODO mine directly 
+# ./minerd -a yespower -o http://127.0.0.1:1867 -u whive -p pass --no-longpoll --no-getwork --no-stratum  --coinbase-addr=YOUR_ADDY -t 3
+
+# TODO mine via a pool
+# ./minerd -a yespower -o stratum+tcp://34.74.4.172:3333 -u WZcQrrYJqrtTXuK8X9aqcQbpbWPdWKf3sL.w1 -t 2
